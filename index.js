4445c0d2e5f79570ee6e553dacff3008f5f10074
Jsheumaker1 <Jsheumaker1@gmail.com>
2021-04-21 13:24:49 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove(main)
+
+const newHeader = document.createElement('h1')
+
+document.body.appendChild (newHeader)
+
+newHeader.id='victory'
+
+victory.innerHTML = 'Jami is the champion'
